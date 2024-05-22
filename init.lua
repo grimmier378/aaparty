b4619d7361a64133606c0db8d9f4e73b23f3416d
grimmier378 <124466615+grimmier378@users.noreply.github.com>
2024-05-21 23:28:41 -0600

    style: wrapping layout
    you can right click to enable sizing and lay he items out in rows / columns by resizing the window.
    
    Right clicking again will reset auto resize to that layout.

--- init.lua
+++ init.lua
@@ -25,7 +25,17 @@ local aSize = true
 --create mailbox for actors to send messages to
 function RegisterActor()
     Actor = actors.register('aa_party', function(message)
-    local MemberEntry = message()
+        local MemberEntry = message()
+        local aaXP = MemberEntry.PctExpAA or 0
+        local aaSetting = MemberEntry.Setting or '0'
+        local who = MemberEntry.Name
+        local pctXP = MemberEntry.PctExp or 0
+        local pts = MemberEntry.Pts or 0
+        local ptsTotal = MemberEntry.PtsTotal or 0
+        local ptsSpent = MemberEntry.PtsSpent or 0
+        local lvlWho = MemberEntry.Level or 0
+        local found = false
+        --New member connected if Hello is true. Lets send them our data so they have it.
         if MemberEntry.Hello then
             Actor:send({mailbox='aa_party'}, {PctExp = PctExp,
             PctExpAA = PctAA,
@@ -39,15 +49,7 @@ function RegisterActor()
             PtsSpent = PtsSpent})
             MemberEntry.Hello = false
         end
-        local aaXP = MemberEntry.PctExpAA or 0
-        local aaSetting = MemberEntry.Setting or '0'
-        local who = MemberEntry.Name
-        local pctXP = MemberEntry.PctExp or 0
-        local pts = MemberEntry.Pts or 0
-        local ptsTotal = MemberEntry.PtsTotal or 0
-        local ptsSpent = MemberEntry.PtsSpent or 0
-        local lvlWho = MemberEntry.Level or 0
-        local found = false
+        -- Check for Execution commands in message and if DoWho is You then execute them.
         if MemberEntry.DoWho ~= nil then
             if MemberEntry.DoWho == ME then
                 local doWhat = MemberEntry.DoWhat
@@ -58,6 +60,7 @@ function RegisterActor()
                 end
             end
         end
+        -- Process the rest of the message into the groupData table.
         for i = 1, #groupData do
             if groupData[i].Name == who then
             groupData[i].PctExpAA = aaXP
@@ -128,63 +131,100 @@ local function AA_Party_GUI(openGUI)
             aSize = not aSize
         end
         if #groupData > 0 then
-            for i = 1, #groupData do
-                if groupData[i] ~= nil then
-                    ImGui.BeginGroup()
-                    ImGui.PushID(groupData[i].Name)
-                    ImGui.SeparatorText("%s (%s)", groupData[i].Name, groupData[i].Level)
-                    ImGui.PushStyleColor(ImGuiCol.PlotHistogram,ImVec4(1, 0.9, 0.4, 0.5))
-                    ImGui.ProgressBar(groupData[i].PctExp/100,ImVec2(165,5),"##PctXP"..groupData[i].Name)
-                    ImGui.PopStyleColor()
-                    ImGui.PushStyleColor(ImGuiCol.PlotHistogram,ImVec4(0.2, 0.9, 0.9, 0.5))
-                    ImGui.ProgressBar(groupData[i].PctExpAA/100,ImVec2(165,5),"##AAXP"..groupData[i].Name)
-                    ImGui.PopStyleColor()
-                    ImGui.PopID()
-                    ImGui.EndGroup()
-                    if expand[groupData[i].Name] == nil then expand[groupData[i].Name] = false end
-                    if ImGui.IsItemHovered() then
-                        if ImGui.IsMouseReleased(0) then
-                            expand[groupData[i].Name] = not expand[groupData[i].Name]
-                        end
+            local windowWidth = imgui.GetWindowWidth()
+            local currentX, currentY = imgui.GetCursorPosX(), imgui.GetCursorPosY()
+            local itemWidth = 170 -- approximate width
+            local padding = 4 -- padding between items
 
-                        ImGui.BeginTooltip()
-                        local tTipTxt = "\t\t"..groupData[i].Name
-                        ImGui.TextColored(ImVec4(1, 1, 1, 1),tTipTxt)
-                        ImGui.Separator()
-                        tTipTxt = string.format("Exp:\t\t\t%.2f %%",groupData[i].PctExp)
-                        ImGui.TextColored(ImVec4(1, 0.9, 0.4, 1),tTipTxt)
-                        tTipTxt = string.format("AA Exp: \t%.2f %%",groupData[i].PctExpAA)
-                        ImGui.TextColored(ImVec4(0.2, 0.9, 0.9, 1),tTipTxt)
-                        tTipTxt = string.format("Avail:  \t\t%d",groupData[i].Pts)
-                        ImGui.TextColored(ImVec4(0, 1, 0, 1),tTipTxt)
-                        tTipTxt = string.format("Spent:\t\t%d",groupData[i].PtsSpent)
-                        ImGui.TextColored(ImVec4(0.9, 0.4, 0.4, 1),tTipTxt)
-                        tTipTxt = string.format("Total:\t\t%d",groupData[i].PtsTotal)
-                        ImGui.TextColored(ImVec4(0.8, 0.0, 0.8, 1.0),tTipTxt)
-                        ImGui.EndTooltip()
-                    end
-                    if expand[groupData[i].Name] then
-                        if ImGui.Button("<##Decrease"..groupData[i].Name) then
-                            Actor:send({mailbox='aa_party'}, {PctExp = PctExp, PctExpAA = PctAA, Level = MeLevel, DoWho = groupData[i].Name, DoWhat = 'Less',Setting = SettingAA, Name = ME, Pts = PtsAA, PtsTotal = PtsTotal, PtsSpent = PtsSpent})
+            for i = 1, #groupData do
+                if i == 1 then currentY = imgui.GetCursorPosY() end
+                if groupData[i] ~= nil then
+                    if expand[groupData[i].Name] == nil then expand[groupData[i].Name] = false end
+                    if currentX + itemWidth > windowWidth +2 then
+                        imgui.NewLine()
+                        currentY = imgui.GetCursorPosY()
+                        currentX = imgui.GetCursorPosX()
+                        -- currentY = imgui.GetCursorPosY()
+                        ImGui.SetCursorPosY(currentY - 20)
+                    else
+                        if i > 1 then
+                            imgui.SameLine()
+                            -- ImGui.SetCursorPosY(currentY)
                         end
-                        ImGui.SameLine()
+                    end
+                    local childY = 68
+                    if not expand[groupData[i].Name] then childY = 42 end
+                    ImGui.PushStyleVar(ImGuiStyleVar.WindowPadding, 2,2)
+                    imgui.BeginChild(groupData[i].Name,165, childY, bit32.bor(ImGuiChildFlags.Border,ImGuiChildFlags.AutoResizeY))
+                    -- Start of grouped Whole Elements
+                    ImGui.BeginGroup()
+                    -- Start of subgrouped Elements for tooltip
+                    imgui.PushID(groupData[i].Name)
+                    imgui.Text("%s (%s)", groupData[i].Name, groupData[i].Level)
+                    imgui.PushStyleColor(ImGuiCol.PlotHistogram, ImVec4(1, 0.9, 0.4, 0.5))
+                    imgui.ProgressBar(groupData[i].PctExp / 100, ImVec2(160, 5), "##PctXP" .. groupData[i].Name)
+                    imgui.PopStyleColor()
+                    imgui.PushStyleColor(ImGuiCol.PlotHistogram, ImVec4(0.2, 0.9, 0.9, 0.5))
+                    imgui.ProgressBar(groupData[i].PctExpAA / 100, ImVec2(160, 5), "##AAXP" .. groupData[i].Name)
+                    imgui.PopStyleColor()
+                    imgui.PopID()
+                    ImGui.EndGroup()
+                    -- end of subgrouped Elements for tooltip begin tooltip
+                    if ImGui.IsItemHovered() then
+                        imgui.BeginTooltip()
+                        local tTipTxt = "\t\t" .. groupData[i].Name
+                        imgui.TextColored(ImVec4(1, 1, 1, 1), tTipTxt)
+                        imgui.Separator()
+                        tTipTxt = string.format("Exp:\t\t\t%.2f %%", groupData[i].PctExp)
+                        imgui.TextColored(ImVec4(1, 0.9, 0.4, 1), tTipTxt)
+                        tTipTxt = string.format("AA Exp: \t%.2f %%", groupData[i].PctExpAA)
+                        imgui.TextColored(ImVec4(0.2, 0.9, 0.9, 1), tTipTxt)
+                        tTipTxt = string.format("Avail:  \t\t%d", groupData[i].Pts)
+                        imgui.TextColored(ImVec4(0, 1, 0, 1), tTipTxt)
+                        tTipTxt = string.format("Spent:\t\t%d", groupData[i].PtsSpent)
+                        imgui.TextColored(ImVec4(0.9, 0.4, 0.4, 1), tTipTxt)
+                        tTipTxt = string.format("Total:\t\t%d", groupData[i].PtsTotal)
+                        imgui.TextColored(ImVec4(0.8, 0.0, 0.8, 1.0), tTipTxt)
+                        imgui.EndTooltip()
+                    end
+                    -- end tooltip
+
+                    -- expanded section for adjusting AA settings
+                    
+                    if expand[groupData[i].Name] then
+                        if imgui.Button("<##Decrease" .. groupData[i].Name) then
+                            Actor:send({mailbox = 'aa_party'}, {PctExp = PctExp, PctExpAA = PctAA, Level = MeLevel, DoWho = groupData[i].Name, DoWhat = 'Less', Setting = SettingAA, Name = ME, Pts = PtsAA, PtsTotal = PtsTotal, PtsSpent = PtsSpent})
+                        end
+                        imgui.SameLine()
                         local tmp = groupData[i].Setting
                         tmp = tmp:gsub("%%", "")
                         local AA_Set = tonumber(tmp) or 0
                         -- this is for my OCD on spacing
                         if AA_Set < 10 then
-                            ImGui.Text("\tAA Setting:   %d", AA_Set)
+                            imgui.Text("\tAA Setting:   %d", AA_Set)
                         elseif AA_Set < 100 then
-                            ImGui.Text("\tAA Setting:  %d", AA_Set)
+                            imgui.Text("\tAA Setting:  %d", AA_Set)
                         else
-                            ImGui.Text("\tAA Setting: %d", AA_Set)
+                            imgui.Text("\tAA Setting: %d", AA_Set)
                         end
-                        ImGui.SameLine()
-                        ImGui.SetCursorPosX(158)
-                        if ImGui.Button(">##Increase"..groupData[i].Name) then
-                            Actor:send({mailbox='aa_party'}, {PctExp = PctExp, PctExpAA = PctAA, Level = MeLevel, DoWho = groupData[i].Name, DoWhat = 'More',Setting = SettingAA, Name = ME, Pts = PtsAA, PtsTotal = PtsTotal, PtsSpent = PtsSpent})
+                        imgui.SameLine()
+                        imgui.SetCursorPosX(ImGui.GetCursorPosX() + 10)
+                        if imgui.Button(">##Increase" .. groupData[i].Name) then
+                            Actor:send({mailbox = 'aa_party'}, {PctExp = PctExp, PctExpAA = PctAA, Level = MeLevel, DoWho = groupData[i].Name, DoWhat = 'More', Setting = SettingAA, Name = ME, Pts = PtsAA, PtsTotal = PtsTotal, PtsSpent = PtsSpent})
                         end
                     end
+                    
+                    ImGui.Separator()
+                    imgui.EndChild()
+                    ImGui.PopStyleVar()
+                    -- End of grouped items
+                    -- Left Click to expand the group for AA settings
+                    if imgui.IsItemHovered() then
+                        if imgui.IsMouseReleased(0) then
+                            expand[groupData[i].Name] = not expand[groupData[i].Name]
+                        end
+                    end
+                    currentX = currentX + itemWidth + padding
                 end
             end
         end
